ed0d7d3166f718e4e9b443a120bc48a34e59fec1
Zack Guard <zacharyguard@gmail.com>
2014-12-12 17:29:22 +0800

    Add wysiwig editor (still WIP)

--- admin/auth/script.js
+++ admin/auth/script.js
@@ -11,24 +11,33 @@ var xhr = function(url,callback) {
 var fileList = document.querySelector(".file-list");
 var fileListItems = document.querySelectorAll(".file-list li");
 
-var codeEditor = document.querySelector("#code-editor");
+var editorElem = document.querySelector("#editor");
 var saveButton = document.querySelector("#save");
 
 [].slice.call(fileListItems).forEach(function(fileListItem){
     fileListItem.addEventListener("click", function(){
         var that = this;
         xhr(location.protocol + "//" + location.host + "/admin/content/" + this.textContent, function(response){
-            codeEditor.value = response;
-            codeEditor.dataset.filename = that.textContent;
+            wysiEditor.focus();
+            wysiEditor.clear();
+            wysiEditor.composer.commands.exec("insertHTML", response);
+            
+            editorElem.dataset.filename = that.textContent;
         });
     });
     
     saveButton.addEventListener("click", function(){
-        if (codeEditor.value) {
-            xhrURL = "/py/update_file.py?data=" + encodeURIComponent(codeEditor.value) + "&fname=" + encodeURIComponent(codeEditor.dataset.filename);
+        var value = wysiEditor.getValue();
+        if (value) {
+            xhrURL = "/py/update_file.py?data=" + encodeURIComponent(value) + "&fname=" + encodeURIComponent(editorElem.dataset.filename);
             xhr(xhrURL, function(r){
                 console.log(r);
             });
         }
     });
+});
+
+var wysiEditor = new wysihtml5.Editor("editor", {
+    toolbar: "toolbar",
+    parserRules:  wysihtml5ParserRules
 });
